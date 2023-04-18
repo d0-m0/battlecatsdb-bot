3cfb980b1e65434497e1970857f135309bc201ba
doーも <91412978+d0-m0@users.noreply.github.com>
2023-04-18 18:37:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,40 +6,47 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
-    print("ready!")
-    print(discord.__version__)
+  print("ready!")
+  print(discord.__version__)
 
 
 @client.event
 async def on_message(message):
-     if message.author.bot:
-        return 
-     if "!db" in message.content:
-         str = ""
+  if message.author.id == "1096017556415987762": return
 
-         search_word = message.content.replace("!db ", "")
+  if message.content.lower().startswith("!db "):
+    str = ""
 
-         pages_num = 3 + 1
+    tar = "html"
 
-         search_word1 = "にゃんこ大戦争DB " + search_word
+    search_word = message.content.lower().replace("!db", "")
 
-         url = f'https://www.google.co.jp/search?hl=ja&num={pages_num}&q={search_word1}'
-         request = requests.get(url)
+    pages_num = 3 + 1
 
-         soup = BeautifulSoup(request.text, "html.parser")
-         search_site_list = soup.select('div.kCrYT > a')
+    search_word1 = search_word + "site:battlecats-db.com/"
 
-         for rank, site in zip(range(1, pages_num), search_site_list):
-             try:
-                 site_title = site.select('h3.zBAuLc')[0].text
-             except IndexError:
-                 site_title = site.select('img')[0]['alt']
-             site_url = site['href'].replace('/url?q=', '')
-             site_url1 = "https://www.google.co.jp/url?esrc=s&q=&rct=j&sa=U&url=" + site_url
-             str += site_title + ": " + site_url1 + "\n"
-         embed = discord.Embed(title=f'【検索ワード】{search_word}',description=str)
-         await message.channel.send(embed=embed)
- 
+    url = f'https://www.google.co.jp/search?hl=ja&num={pages_num}&q={search_word1}'
+    request = requests.get(url)
+
+    soup = BeautifulSoup(request.text, "html.parser")
+    search_site_list = soup.select('div.kCrYT > a')
+
+    for rank, site in zip(range(1, pages_num), search_site_list):
+      try:
+        site_title = site.select('h3.zBAuLc')[0].text
+      except IndexError:
+        site_title = site.select('img')[0]['alt']
+      site_url = site['href'].replace('/url?q=', '')
+      idx = site_url.find(tar)
+      site_url2 = site_url[:idx + 4]
+      str += site_title + "\n" + site_url2 + "\n"
+    embed = discord.Embed(title=f'【検索ワード】{search_word}',
+                          description=str,
+                          color=discord.Colour.yellow())
+    await message.channel.send(embed=embed)
+    
+
 client.run("TOKEN")
